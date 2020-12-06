a3c17e997543d5ad80567392f98bb68ddbce420a
Vlad Adrian <vladadrian1361@gmail.com>
2020-12-06 18:17:29 +0200

    Lucrarea 6: Items 13, 14, 15

---
+++ Lucrarea6/VladAdrian/build/CMakeFiles/lucrarea6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lucrarea6.dir/main.cpp.o"
+  "lucrarea6"
+  "lucrarea6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lucrarea6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
