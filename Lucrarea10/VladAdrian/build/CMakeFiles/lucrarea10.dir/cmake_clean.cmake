caf9a13f7916e3689cab418e15d791e93eb3f6c7
Vlad Adrian <vladadrian1361@gmail.com>
2020-12-12 18:42:24 +0200

    Lucrarea 10. Nu am inteles exact ce trebuie facut pentru functia de vandut la clasa gaini

---
+++ Lucrarea10/VladAdrian/build/CMakeFiles/lucrarea10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lucrarea10.dir/main.cpp.o"
+  "lucrarea10"
+  "lucrarea10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lucrarea10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
